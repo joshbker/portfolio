fa4d45714863979597203e676f63bd67fff21143
devous <43449531+devous@users.noreply.github.com>
2023-02-17 23:58:52 +0000

    Actually fix "take a look"

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -27,8 +27,8 @@
 <div class="flex justify-between space-y-10 space-x-16 mt-10">
     <div class="flex flex-col">
         <h2 class="font-heading text-white text-2xl">Check out my experience with different languages, frameworks, tools<br>and more.</h2>
-        <a href="/skills" class="font-heading text-[#9898A5] hover:text-white text-lg group transition-colors z-20 mt-3 items-start">
-            Take a look
+        <a href="/skills" class="font-heading text-[#9898A5] hover:text-white text-lg group transition-colors z-20 mt-3 flex items-center space-x-2 self-start">
+            <span>Take a look</span>
             <svg class="fill-[#9898A5] group-hover:fill-white h-5 inline-block ml-1 group-hover:translate-x-1 transition-all" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></svg>
         </a>
         <div class="-mt-4 h-[22rem]">
