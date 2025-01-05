78e7f56bd9d640cdfa08c140cb32cac2382f75b1
Josh <43449531+joshbker@users.noreply.github.com>
2025-01-05 21:41:28 +0000

    modify projects and experience

--- src/lib/component/TileBox.svelte
+++ src/lib/component/TileBox.svelte
@@ -2,9 +2,9 @@
     import Tile from "$lib/component/Tile.svelte";
     import tailwindLogo from '$lib/image/tailwindcss-mark.svg';
     import svelteLogo from '$lib/image/svelte.png';
-    import tsLogo from '$lib/image/ts.png';
     import jsLogo from '$lib/image/js.png';
     import javaLogo from '$lib/image/java.png';
+    import kotlinLogo from '$lib/image/kotlin.png';
     import figmaLogo from '$lib/image/figma.png';
     import mongoLogo from '$lib/image/mongodb.png';
     import jetbrainsLogo from '$lib/image/jetbrains.png';
@@ -13,30 +13,30 @@
 <div class="">
     <div>
         <div class="-translate-x-[9rem] translate-y-[2.75rem]">
-            <Tile logo={svelteLogo} name="Svelte" shadowColor={[255, 62, 0]} />
+            <Tile logo={kotlinLogo} name="Kotlin" shadowColor={[255, 62, 0]} />
         </div>
         <div class="">
-            <Tile logo={tailwindLogo} name="Tailwind" shadowColor={[56, 189, 248]} />
+            <Tile logo={javaLogo} name="Java" shadowColor={[56, 189, 248]} />
         </div>
         <div class="translate-x-[9rem] -translate-y-[2.75rem]">
-            <Tile logo={figmaLogo} name="Figma" shadowColor={[10, 207, 131]} />
+            <Tile logo={svelteLogo} name="Svelte" shadowColor={[10, 207, 131]} />
         </div>
         <div class="translate-x-[18rem] -translate-y-[5.5rem]">
-            <Tile logo={javaLogo} name="Java" shadowColor={[240, 63, 51]} />
+            <Tile logo={jsLogo} name="JavaScript" shadowColor={[240, 63, 51]} />
         </div>
     </div>
     <div class="translate-y-[6.5rem] translate-x-[6rem]">
         <div class="-translate-x-[9rem] translate-y-[2.75rem]">
-            <Tile logo={mongoLogo} name="MongoDB" shadowColor={[10, 207, 131]} />
+            <Tile logo={tailwindLogo} name="Tailwind" shadowColor={[10, 207, 131]} />
         </div>
         <div class="">
-            <Tile logo={jsLogo} name="JavaScript" shadowColor={[240, 219, 79]} />
+            <Tile logo={figmaLogo} name="Figma" shadowColor={[240, 219, 79]} />
         </div>
         <div class="translate-x-[9rem] -translate-y-[2.75rem]">
-            <Tile logo={jetbrainsLogo} name="JetBrains" shadowColor={[254, 49, 93]} />
+            <Tile logo={mongoLogo} name="MongoDB" shadowColor={[254, 49, 93]} />
         </div>
         <div class="translate-x-[18rem] -translate-y-[5.5rem]">
-            <Tile logo={tsLogo} name="TypeScript" shadowColor={[49, 120, 198]} />
+            <Tile logo={jetbrainsLogo} name="IntelliJ" shadowColor={[49, 120, 198]} />
         </div>
     </div>
 </div>
